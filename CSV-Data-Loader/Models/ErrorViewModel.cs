ecd34078a98afd4a42922d4557a09a6f3b9fd35c
wolfgang-xue <wolfgangxue@gmail.com>
2019-03-28 20:17:44 +1100

    Add project files.

---
+++ CSV-Data-Loader/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CSV_Data_Loader.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
